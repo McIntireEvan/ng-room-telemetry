ab052c4b8be874c47365e0553387eb45d11d91fb
Evan McIntire <mcintire.evan@gmail.com>
2017-04-11 02:04:56 -0400

    Add socket.io, begin working on chart frontend

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return render_template('index.html')
 
-if __name__ == "__main__"
-	app.run()
+@socketio.on('connect')
+def connect_handle():
+	print("User connected")
+
+if __name__ == "__main__":
+	socketio.run(app)
